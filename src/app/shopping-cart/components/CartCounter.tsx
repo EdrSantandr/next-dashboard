7304932c25326744ee751ff1f6344c39817f7869
EdrSantandr <eder.santander.123@gmail.com>
2026-01-18 10:29:00 -0500

    Recommended changes by visual

--- src/app/shopping-cart/components/CartCounter.tsx
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -17,11 +17,11 @@ export const CartCounter = ({ value = 0 }: CartCounterProps) => {
             <div className="flex">
                 <button
                     onClick={() => setCount(count + 1)}
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">+1</button>
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2">+1</button>
 
                 <button
                     onClick={() => setCount(count - 1)}
-                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">-1</button>
+                    className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2">-1</button>
             </div>
         </>
     )
